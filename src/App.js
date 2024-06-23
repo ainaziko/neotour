42e25225d836685ff2176f8817e1bb23dda9e494
Ainazik Momunalieva <58284183+ainaziko@users.noreply.github.com>
2024-06-23 21:00:53 +0600

    added alts

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Routes, Route, Link} from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
 import HomePage from "./pages/home/HomePage";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
