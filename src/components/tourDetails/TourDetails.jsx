70faca59291a576f4dfae3aa6f1c3c2da61645de
Ainazik Momunalieva <58284183+ainaziko@users.noreply.github.com>
2024-06-26 15:24:31 +0600

    added go back btn to single page, updated layouts, added anonymous icon for users with no photo

--- src/components/tourDetails/TourDetails.jsx
+++ src/components/tourDetails/TourDetails.jsx
@@ -1,10 +1,13 @@
 import React, { useEffect, useState, useCallback } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { tourDetailsApi } from "../../api";
 import styles from './TourDetails.module.css';
 import vector from '../../assets/Vector.svg';
+import goBackArrow from '../../assets/goBackArrow.svg'
+import Review from "../review/Review";
 
 const TourDetails = () => {
+    const navigate = useNavigate();
     const { id } = useParams();
     const [tour, setTour] = useState(null);
     const [loading, setLoading] = useState(true);
@@ -34,25 +37,34 @@ const TourDetails = () => {
 
     return (
         <div className={styles.details}>
+            <button className={styles.goBackBtn} onClick={() => navigate(-1)}><img className={styles.goBackArrow} src={goBackArrow}/>Go back</button> 
+            
             <img className={styles.detailsImg} src={tour.thumbnail} alt="tour img" />
-            <div className={styles.detailsInfo}>
-                <p className={styles.title}>{tour.name}</p>
-                <p className={styles.location}>
-                    <span className={styles.vector}>
-                        <img src={vector} alt="location icon" />
-                    </span>
-                    {tour.location}
-                </p>
-                <p className={styles.descriptionTitle}>Description</p>
-                <p className={styles.description}>{tour.description}</p>
-                <p className={styles.reviewsTitle}>Reviews</p>
-                {tour.reviews.map((review, index) => (
-                    <div key={index}>
-                        <p className={styles.reviewerName}>{review.reviewer_name}</p>
-                        <img className={styles.reviewerImg} src={review.reviewer_photo} alt="reviewer" />
-                        <p className={styles.reviewTxt}>{review.review_text}</p>
-                    </div>
-                ))}
+            <div className={styles.info}>
+                <div className={styles.detailsInfo}>
+                    <p className={styles.title}>{tour.name}</p>
+                    <p className={styles.location}>
+                        <span className={styles.vector}>
+                            <img src={vector} alt="location icon" />
+                        </span>
+                        {tour.location}
+                    </p>
+                    <p className={styles.subTitle}>Description</p>
+                    <p className={styles.description}>{tour.description}</p>
+                    {tour.reviews === null || tour.reviews.size === 0}
+                    {(!tour.reviews || tour.reviews.length === 0) ? (
+                        <p className={styles.comment}>No reviews yet</p>
+                            ) : (
+                                <>
+                                    <p className={styles.subTitle}>Reviews</p>
+                                    {tour.reviews.map((review, index) => (
+                                        <div key={index}>
+                                            <Review name={review.reviewer_name} photo={review.reviewer_photo} review={review.review_text}/>
+                                        </div>
+                                    ))}
+                                </>
+                            )}
+                </div>
                 <div className={styles.btnContainer}>
                     <button className={styles.bookBtn}>Book now</button>
                 </div>
